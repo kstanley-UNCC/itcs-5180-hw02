eef4a596a33fbd506b201e38d02113e270d8d354
Ken Stanley <dohpaz@gmail.com>
2022-09-08 21:17:18 -0400

    Add .idea folder to project

---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+/*
+ * Group 22 Homework 02
+ * ${NAME}.java
+ * Ken Stanley & Stephanie Karp
+ */
+ 
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public class ${NAME} {
+}
