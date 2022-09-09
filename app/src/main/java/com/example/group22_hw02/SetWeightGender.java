939a73290ea2af4678983820be59064cb0936240
Ken Stanley <dohpaz@gmail.com>
2022-09-08 22:15:56 -0400

    Add key to strings.xml

--- app/src/main/java/com/example/group22_hw02/SetWeightGender.java
+++ app/src/main/java/com/example/group22_hw02/SetWeightGender.java
@@ -51,7 +51,7 @@ public class SetWeightGender extends AppCompatActivity {
 
             setWeight();
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            intent.putExtra("profile", new Profile(userWeight, gender, genderConstant));
+            intent.putExtra(getString(R.string.intent_profile), new Profile(userWeight, gender, genderConstant));
 
             setResult(RESULT_OK, intent);
 
