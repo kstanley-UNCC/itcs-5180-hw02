ea49a8e4355c0a250d97ab99c155f5d568ab2c56
Ken Stanley <dohpaz@gmail.com>
2022-09-09 21:17:19 -0400

    Throw exception if result code is unknown

--- app/src/main/java/com/example/group22_hw02/activities/MainActivity.java
+++ app/src/main/java/com/example/group22_hw02/activities/MainActivity.java
@@ -100,6 +100,8 @@ public class MainActivity extends AppCompatActivity implements Comparable<MainAc
                             }
                         });
                         break;
+                    default:
+                        throw new IllegalArgumentException(getString(R.string.exception_illegal_argument_unknown_result_code, result.getResultCode()));
                 }
             }
         }
