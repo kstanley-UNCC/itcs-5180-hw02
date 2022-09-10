997b7369fdd99fea33297b3c775670bec30bc9c8
Ken Stanley <dohpaz@gmail.com>
2022-09-09 21:53:39 -0400

    Add getGenderConstant() method to Profile class

--- app/src/main/java/com/example/group22_hw02/Profile.java
+++ app/src/main/java/com/example/group22_hw02/Profile.java
@@ -1,6 +1,9 @@
 package com.example.group22_hw02;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Profile implements Serializable {
     public final int weight;
@@ -12,5 +15,15 @@ public class Profile implements Serializable {
     public Profile(int weight, String gender) {
         this.weight = weight;
         this.gender = gender;
+    }
+
+    public double getGenderConstant(AppCompatActivity app) {
+        if (Objects.equals(app.getString(R.string.gender_group_female), gender)) {
+            return BAC_GENDER_FEMALE;
+        } else if (Objects.equals(app.getString(R.string.gender_group_male), gender)) {
+            return BAC_GENDER_MALE;
+        } else {
+            throw new IllegalStateException(app.getString(R.string.exception_illegal_state_gender));
+        }
     }
 }
